5014938f55377b08127fc48ea7d5f87603019d8e
tiankonglanlande <zswdxl_111@sina.com>
2018-11-11 17:30:31 +0800

    spring boot 中使用servlet

--- springboot-servlet/src/main/java/com/tiankonglanlande/cn/springboot/servlet/servlet/MyServlet.java
+++ springboot-servlet/src/main/java/com/tiankonglanlande/cn/springboot/servlet/servlet/MyServlet.java
@@ -1,6 +1,5 @@
 package com.tiankonglanlande.cn.springboot.servlet.servlet;
 
-import javax.servlet.AsyncContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,24 +7,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = "/my/servlet",asyncSupported = true)
+@WebServlet(urlPatterns = "/my/servlet")
 public class MyServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doGet(req, resp);
-
-        AsyncContext asyncContext = req.getAsyncContext();
-        asyncContext.start(()->{
-            try {
-                resp.getWriter().print("hello servlet!");
-                asyncContext.complete();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-        });
-
-
+        resp.getWriter().print("hello this is MyServlet!");
     }
 }
