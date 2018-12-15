2e88edc39f7e17a13dd3c59d2d093d70601b344e
tiankonglanlande <zswdxl_111@sina.com>
2018-12-15 12:09:25 +0800

    添加SpringBoot系列8-使用jasypt加密配置文件内容简单版

---
+++ springboot-jasypt-simple/src/main/java/com/tiankonglanlande/cn/springboot/jasypt/simple/JasyptSimpleApplication.java
@@ -0,0 +1,14 @@
+package com.tiankonglanlande.cn.springboot.jasypt.simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasyptSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JasyptSimpleApplication.class, args);
+	}
+
+}
+
