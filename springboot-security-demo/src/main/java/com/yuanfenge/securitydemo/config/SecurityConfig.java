66db042d37bc0d5dfdf1713d4bfb61e5adbc4359
tiankonglanlande <_google1985>
2023-09-15 12:01:57 +0800

    完善springboot-security-demo

--- springboot-security-demo/src/main/java/com/yuanfenge/securitydemo/config/SecurityConfig.java
+++ springboot-security-demo/src/main/java/com/yuanfenge/securitydemo/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.yuanfenge.securitydemo.config;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,34 +12,59 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Joe Grandja
  */
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    // @formatter:off
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests(authorize -> authorize
-                        .antMatchers("/css/**", "/index").permitAll()
-                        .antMatchers("/user/**").hasRole("USER")
+        http.authorizeRequests(authorize -> authorize
+                        /*配置放行资源或页面*/
+                        .antMatchers("/css/**","/js/**", "/index").permitAll()
+                        /*配置认证资源，必需是USER角色*/
+                        .antMatchers("/user/**")
+                        .hasRole("USER")
                 )
                 .formLogin(formLogin -> formLogin
+                        //配置自定登录页面，不配置则为security默认登录页面
                         .loginPage("/login")
                         .failureUrl("/login-error")
                 );
     }
-    // @formatter:on
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
 
     @Bean
     public UserDetailsService userDetailsService() {
-        UserDetails userDetails = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
+        // TODO 模拟数据库获取用户数据
+        List dbUserList = new ArrayList();
+
+        UserDetails aa = User.withDefaultPasswordEncoder()//设置密码为默认编码
+                .username("aa")
+                .password("aa")
                 .roles("USER")
                 .build();
-        return new InMemoryUserDetailsManager(userDetails);
+        UserDetails bb = User.withDefaultPasswordEncoder()
+                .username("bb")
+                .password("bb")
+                .roles("admin", "user")
+                .build();
+        dbUserList.add(aa);
+        dbUserList.add(bb);
+
+        /*将登录信息保存到缓存*/
+        //return new InMemoryUserDetailsManager(aa,bb);
+        return new InMemoryUserDetailsManager(dbUserList);
     }
 }
--- springboot-security-demo/src/main/java/com/yuanfenge/securitydemo/controller/MainController.java
+++ springboot-security-demo/src/main/java/com/yuanfenge/securitydemo/controller/MainController.java
@@ -1,38 +1,85 @@
 package com.yuanfenge.securitydemo.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * @author Joe Grandja
+ * @author yuanfenge
  */
+@Slf4j
 @Controller
 public class MainController {
 
-    @RequestMapping("/")
-    public String root() {
-        return "redirect:/index";
-    }
-
-    @RequestMapping("/index")
-    public String index() {
+    /**
+     * 首页
+     * @param model
+     * @return
+     */
+    @RequestMapping({"/","/index"})
+    public String root(Model model) {
+        log.info("=========> 请求地址是：/index");
+        getUserInfo(model);
         return "index";
     }
 
+    /**
+     * 获取用户信息
+     * @param model
+     */
+    public void getUserInfo(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        /*获取用户信息*/
+        if (authentication != null && authentication.isAuthenticated()) {
+            Object principal = authentication.getPrincipal();
+            if (principal instanceof UserDetails) {
+                UserDetails userDetails = (UserDetails) principal;
+                String username = userDetails.getUsername();
+                String password = userDetails.getPassword();
+                // TODO 可以获取其他用户信息
+
+
+                model.addAttribute("userDetails",userDetails);
+            }
+        }
+    }
+
+    /**
+     * 用户中心页
+     * @param model
+     * @return
+     */
     @RequestMapping("/user/index")
-    public String userIndex() {
+    public String userIndex(Model model) {
+        log.info("=========> 请求地址是：/user/index");
+        getUserInfo(model);
+
         return "user/index";
     }
 
+    /**
+     * 自定义的登录页面
+     * @return
+     */
     @RequestMapping("/login")
     public String login() {
+        log.info("=========> 请求地址是：自定义的登录页面/login");
         return "login";
     }
 
+    /**
+     * 错误后跳转到登录页面
+     * @param model
+     * @return
+     */
     @RequestMapping("/login-error")
     public String loginError(Model model) {
-        model.addAttribute("loginError", true);
+        log.info("=========> 请求地址是：/login-error");
+        model.addAttribute("登录错误请重新登录！", true);
         return "login";
     }
 
