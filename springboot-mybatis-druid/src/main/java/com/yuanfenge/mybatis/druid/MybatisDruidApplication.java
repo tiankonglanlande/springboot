fdbf8fde81870b953e3da37b9b82e3135054ae49
tiankonglanlande <_google1985>
2023-09-16 17:29:23 +0800

    springboot-mybatis-druid加入druid监测的yml配置，修复之前的包名修改为修改完整的问题

--- springboot-mybatis-druid/src/main/java/com/yuanfenge/mybatis/druid/MybatisDruidApplication.java
+++ springboot-mybatis-druid/src/main/java/com/yuanfenge/mybatis/druid/MybatisDruidApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.yuanfenge.springboot.mybatis.mapper")
+@MapperScan("com.yuanfenge.mybatis.druid.dao")
 public class MybatisDruidApplication {
 
     public static void main(String[] args) {
