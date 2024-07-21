6ac7b76a66e99fca94aecea1e5508b8a178ca139
tiankonglanlande <_google1985>
2024-07-21 09:39:03 +0800

    修改@Import中RetryAspect.class重复导入

--- springboot-starter-retry/src/main/java/com/yuanfenge/RetryAutoConfiguration.java
+++ springboot-starter-retry/src/main/java/com/yuanfenge/RetryAutoConfiguration.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Import;
  * @date: 2024/7/20
  **/
 @Configuration
-@Import({RetryAspect.class,RetryAspect.class, RetryProperties.class})
+@Import({RetryAspect.class, RetryProperties.class})
 public class RetryAutoConfiguration {
 
 }
